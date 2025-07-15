9aaab097d5d4f8849664717c2434e2fe3a928c6d
kl-ong <ongkokle@gmail.com>
2025-07-15 03:22:51 +0000

    Reset to remove key

--- app.py
+++ app.py
@@ -1,25 +1,77 @@
 from flask import Flask, render_template, request
+from groq import Groq
 import joblib
+import os
+
+os.environ['GROQ_API_KEY'] = os.getenv("groq")
 
 app = Flask(__name__)
 
+#----------------------------------------------
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
-# @app.route("/prediction",methods=["GET","POST"])
-# def prediction():
-#     q = float(request.form.get("q"))
-#     return(render_template("prediction.html",r=(-50.6*q)+90.2))
-
+#----------------------------------------------
 @app.route("/main",methods=["GET","POST"])
 def main():  
-    # q = float(request.form.get("q"))
     return(render_template("main.html"))
-           
+
+#----------------------------------------------
 @app.route("/dbs",methods=["GET","POST"])
 def dbs():  
     return(render_template("dbs.html"))
+
+#----------------------------------------------
+@app.route("/llama",methods=["GET","POST"])
+def llama():  
+    return(render_template("llama.html"))
+
+#----------------------------------------------
+@app.route("/llama_reply",methods=["GET","POST"])
+def llama_reply():
+    
+    q = request.form.get("q")
+
+    # load model
+    client = Groq()
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role":"user",
+                "content":q
+            }
+        ]
+    )
+
+    return(render_template("llama_reply.html", r=completion.choices[0].message.content))
+
+#----------------------------------------------
+@app.route("/deepseek",methods=["GET","POST"])
+def deepseek():  
+    return(render_template("deepseek.html"))
+
+#----------------------------------------------
+@app.route("/deepseek_reply",methods=["GET","POST"])
+def deepseek_reply():
+    
+    q = request.form.get("q")
+
+    # load model
+    client = Groq()
+    completion = client.chat.completions.create(
+        model="deepseek-r1-distill-llama-70b",
+        messages=[
+            {
+                "role":"user",
+                "content":q
+            }
+        ]
+    )
+
+    return(render_template("deepseek_reply.html", r=completion.choices[0].message.content))
+#----------------------------------------------
 
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
@@ -32,6 +84,8 @@ def prediction():
     pred = model.predict([[q]])
 
     return(render_template("prediction.html",r=pred))
+
+#----------------------------------------------
 
 
            
